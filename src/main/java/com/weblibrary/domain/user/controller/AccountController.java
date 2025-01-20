d3514c06b8655f898adc54b827c24b084495d356
cheoljundev <cheoljundev@gmail.com>
2025-01-20 22:12:14 +0900

    feat: JoinValidator 추가, Validator 중복 처리

--- src/main/java/com/weblibrary/domain/user/controller/AccountController.java
+++ src/main/java/com/weblibrary/domain/user/controller/AccountController.java
@@ -1,10 +1,11 @@
 package com.weblibrary.domain.user.controller;
 
+import com.weblibrary.domain.user.model.JoinUserDto;
 import com.weblibrary.domain.user.model.LoginUserDto;
 import com.weblibrary.domain.user.model.User;
 import com.weblibrary.domain.user.service.UserService;
+import com.weblibrary.domain.user.validation.JoinValidator;
 import com.weblibrary.domain.user.validation.LoginValidator;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,9 +16,10 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * 유저 회원가입 컨트롤러, GET, POST에 따라 다르게 동작.
@@ -29,38 +31,52 @@ public class AccountController {
 
     private final UserService userService;
     private final LoginValidator loginValidator;
+    private final JoinValidator joinValidator;
 
-    @InitBinder
-    public void init(WebDataBinder dataBinder) {
+    @InitBinder("joinUser")
+    public void initJoinBinder(WebDataBinder dataBinder) {
+        dataBinder.addValidators(joinValidator);
+    }
+
+    @InitBinder("loginUser")
+    public void initLoginBinder(WebDataBinder dataBinder) {
         dataBinder.addValidators(loginValidator);
     }
 
     /* join form 보여주기 */
     @GetMapping("/join")
-    public String joinForm() {
+    public String joinForm(Model model) {
+        model.addAttribute("joinUser", new JoinUserDto());
         return "home/join";
     }
 
     /* 회원가입 처리하기 */
     @PostMapping("/join")
-    public void join(@RequestParam("username") String username, @RequestParam("password") String password, HttpServletResponse response) throws IOException {
+    public String join(@Validated @ModelAttribute("joinUser") JoinUserDto user, BindingResult bindingResult) {
 
-        // Service 계층의 join메서드로 가입 처리
-        userService.join(username, password);
+        log.debug("objectName={}", bindingResult.getObjectName());
+        log.debug("target={}", bindingResult.getTarget());
+
+        log.debug("Input User DTO: {}", user);
+
+        if (bindingResult.hasErrors()) {
+            log.debug("errors={}", bindingResult);
+            return "home/join";
+        }
 
         // 회원가입 후에 홈으로 리다이렉트
-        response.sendRedirect("/");
+        return "redirect:/";
     }
 
     @GetMapping("/login")
     public String loginForm(Model model) {
-        model.addAttribute("user", new LoginUserDto());
+        model.addAttribute("loginUser", new LoginUserDto());
         return "home/login";
     }
 
     /* 로그인 처리하기 */
     @PostMapping("/login")
-    public String login(@Validated @ModelAttribute("user") LoginUserDto user, BindingResult bindingResult) {
+    public String login(@Validated @ModelAttribute("loginUser") LoginUserDto user, BindingResult bindingResult) {
 
         log.debug("objectName={}", bindingResult.getObjectName()); // loginUserDto로 나오고 있었다. @ModelAttribute("user")로 해결
         log.debug("target={}", bindingResult.getTarget()); // 정상적으로 LoginUserDto 인스턴스를 찾아옴.
---
+++ src/main/java/com/weblibrary/domain/user/model/JoinUserDto.java
@@ -0,0 +1,9 @@
+package com.weblibrary.domain.user.model;
+
+import lombok.Data;
+
+@Data
+public class JoinUserDto {
+    private String username;
+    private String password;
+}
