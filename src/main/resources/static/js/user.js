50fbcc71498ed9b84e878731927c7710fd95bf7d
cheoljundev <cheoljundev@gmail.com>
2025-01-22 05:20:46 +0900

    fix: 로그아웃 alert 버그 수정

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -3,7 +3,7 @@ import {fetchRequest, handleError} from "./util.js";
 export const signout = async () => {
     try {
         const data = await fetchRequest("/signout", "POST");
-        alert("결과 : " + data);
+        alert("결과 : " + data.message);
         location.href = "/";
     } catch (e) {
         handleError(e);
