b5080af5e4c8665ff3276f385e5902dc5d017dc3
cheoljundev <cheoljundev@gmail.com>
2025-02-02 00:11:12 +0900

    feat: JDBC 커넥션 연결을 위한 클래스

---
+++ src/main/java/com/weblibrary/web/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package com.weblibrary.web.connection;
+
+public interface ConnectionConst {
+    String URL = "jdbc:h2:tcp://localhost/~/weblibrary";
+    String USERNAME = "sa";
+    String PASSWORD = "";
+}
