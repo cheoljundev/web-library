046e982f2723147100bb85a7df7e6307a8dd1f56
cheoljundev <cheoljundev@gmail.com>
2025-02-11 00:59:24 +0900

    불필요한 mapper 삭제

--- src/main/java/com/weblibrary/web/interceptor/RestLoginCheckInterceptor.java
+++ src/main/java/com/weblibrary/web/interceptor/RestLoginCheckInterceptor.java
@@ -1,6 +1,5 @@
 package com.weblibrary.web.interceptor;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.weblibrary.web.SessionConst;
 import com.weblibrary.web.exception.UnauthorizedAccessException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -11,8 +10,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class RestLoginCheckInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute(SessionConst.LOGIN_USER) == null) {
             throw new UnauthorizedAccessException("로그인해주세요.");
