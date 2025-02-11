dd8570a7d59e699821102eb41f6526459b4e6e8e
cheoljundev <cheoljundev@gmail.com>
2025-02-12 03:01:46 +0900

    test sql에 description default '' 추가

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -23,7 +23,7 @@ book_id BIGINT PRIMARY KEY AUTO_INCREMENT,
 book_name VARCHAR(255) NOT NULL,
 author VARCHAR(255) NOT NULL,
 isbn VARCHAR(255) NOT NULL UNIQUE,
-description varchar(255) NOT NULL,
+description varchar(255) NOT NULL DEFAULT '',
 rented BOOLEAN NOT NULL DEFAULT FALSE
 );
 
