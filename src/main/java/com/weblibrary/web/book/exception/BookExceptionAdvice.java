a181af96ad115480107e6d3fbadf45fa9181e474
cheoljundev <cheoljundev@gmail.com>
2025-02-20 01:03:31 +0900

    fix: 책 수정 에러 메시지 수정

--- src/main/java/com/weblibrary/web/book/exception/BookExceptionAdvice.java
+++ src/main/java/com/weblibrary/web/book/exception/BookExceptionAdvice.java
@@ -4,30 +4,45 @@ import com.weblibrary.domain.book.exception.DuplicateIsbnException;
 import com.weblibrary.domain.book.exception.NotFoundBookCoverException;
 import com.weblibrary.domain.book.exception.NotFoundBookException;
 import com.weblibrary.web.response.ErrorResponse;
-import com.weblibrary.web.response.ErrorResponseUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class BookExceptionAdvice {
 
-    private final ErrorResponseUtils errorResponseUtils;
-
     @ExceptionHandler(NotFoundBookException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundBookException() {
-        return errorResponseUtils.handleNotFoundErrors("book");
+        Map<String, String> errors = new HashMap<>();
+        errors.put("root", "찾을 수 없는 책입니다.");
+        return ResponseEntity.badRequest().body(ErrorResponse.builder()
+                .code("not found book")
+                .message("책을 찾을 수 없습니다.")
+                .errors(errors).build());
     }
 
     @ExceptionHandler(NotFoundBookCoverException.class)
     public ResponseEntity<ErrorResponse> handleNotFoundBookCoverException() {
-        return errorResponseUtils.handleNotFoundErrors("cover");
+        Map<String, String> errors = new HashMap<>();
+        errors.put("root", "찾을 수 없는 책 표지입니다.");
+        return ResponseEntity.badRequest().body(ErrorResponse.builder()
+                .code("not found book cover")
+                .message("책표지를 찾을 수 없습니다.")
+                .errors(errors).build());
     }
 
     @ExceptionHandler(DuplicateIsbnException.class)
     public ResponseEntity<ErrorResponse> DuplicateIsbnException(DuplicateIsbnException e) {
-        return errorResponseUtils.handleExceptionError(e, "isbn");
+        Map<String, String> errors = new HashMap<>();
+        errors.put("isbn", e.getMessage());
+        return ResponseEntity.badRequest().body(ErrorResponse.builder()
+                .code("duplicated isbn")
+                .message("책 수정 실패")
+                .errors(errors).build());
     }
 }
